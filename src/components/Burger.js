7b92fa561694fccf4ee954cb3ae9cd7c37718966
Mbaye <mmbaye1@hotmail.fr>
2022-01-09 21:39:16 +0100

    Ajout des images du burger

--- src/components/Burger.js
+++ src/components/Burger.js
@@ -1,13 +1,19 @@
 import React from "react";
 import "../style/burger.css";
+import top from "../assets/top.jpg";
+import bottom from "../assets/bottom.jpg";
 
 export default function Burger() {
   return (
     <div>
       <div className="ingredients">
-        <div className="top">img</div>
+        <div className="top">
+          <img src={top} alt={top} />
+        </div>
         <p className="comp">composition</p>
-        <div className="bottom">img</div>
+        <div className="bottom">
+          <img src={bottom} alt={bottom} />
+        </div>
       </div>
       <div className="menu">
         <h2>Salade</h2>
